3754592c91b8f08b24450343519446f1b0e8d007
Yakov Zaytsev <me@yakovzaytsev.com>
2018-10-14 15:35:49 +0300

    get command line arguments closes #1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 3 {
+        println!("Usage: {} filename struct", args[0]);
+        std::process::exit(1);
+    }
+
+    println!("Hello, {}!", args[1]);
 }
