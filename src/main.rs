bffb7bbb6ba9cb25ed54cc4cf77b3c02a8bdc6e6
Yakov Zaytsev <me@yakovzaytsev.com>
2018-10-14 16:29:36 +0300

    read C file #2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-fn main() {
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() -> std::io::Result<()> {
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 3 {
         println!("Usage: {} filename struct", args[0]);
@@ -6,4 +9,11 @@ fn main() {
     }
 
     println!("Hello, {}!", args[1]);
+
+    let mut f = File::open(&args[1])?;
+    let mut byte_vec = vec![];
+    f.read_to_end(&mut byte_vec)?;
+
+    println!("{:?}", byte_vec);
+    Ok(())
 }
